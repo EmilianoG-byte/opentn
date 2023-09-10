fix projection and parametrization of gradient and hessian functions

opentn/stiefel.py
@@ -8,7 +8,7 @@ from jax import config
 config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 import numpy as np
-from opentn.transformations import vectorize
+from opentn.transformations import factorize_psd_truncated
 
 from scipy.linalg import fractional_matrix_power
 
@@ -19,6 +19,19 @@ def project(X:np.ndarray, Z:np.ndarray):
     From https://arxiv.org/pdf/2112.05176.pdf eq.26
     """
     return Z - 0.5 * X @ (X.conj().T @ Z + Z.conj().T @ X)
+
+def symmetrize(A:np.ndarray):
+    """
+    Symmetrize a matrix by projecting it onto the symmetric subspace.
+    """
+    return 0.5 * (A + A.conj().T)
+
+
+def antisymmetrize(A:np.ndarray):
+    """
+    Antisymmetrize a matrix by projecting it onto the antisymmetric (skew-symmetric) subspace.
+    """
+    return 0.5 * (A- A.conj().T)
 
 def metric(delta1:np.ndarray, delta2:np.ndarray, X:np.ndarray):
     """
@@ -41,6 +54,65 @@ def get_unit_matrices(ops:list[np.ndarray]):
         tangent_vector[0,0] = 1 
         unit_matrices.append(tangent_vector)
     return unit_matrices
+
+def get_orthogonal_complement(X:np.ndarray):
+    """
+    Get the orthogonal complement matrix of X such that
+
+    [X, X_comp].conj().T @ [X, X_comp] = I
+
+    see 'notation' in https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=984753
+    """
+    n, p = X.shape
+    P_x = X @ X.conj().T 
+    P_x_comp = np.eye(n) - P_x
+    return factorize_psd_truncated(P_x_comp, chi_max=n-p)
+
+
+def get_symmetric(X:np.ndarray, Z:np.ndarray):
+    """
+    Get the symmetric component A from a general matrix Z
+    
+    Decomposition Z = X @ A + X_comp @ B + X @ C
+
+    From https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=984753 lemma 8.
+    """
+    return antisymmetrize(X.conj().T@Z)
+
+def get_arbitrary(X_comp:np.ndarray, Z:np.ndarray, X:np.ndarray=None, Px_comp:np.ndarray=None):
+    """
+    Get the arbitrary component B from a general matrix Z
+    
+    Decomposition Z = X @ A + X_comp @ B + X @ C
+
+    From https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=984753 lemma 8.
+    """
+    if Px_comp is None and X is not None:
+        n,p = X.shape
+        Px_comp = np.eye(n) - X @ X.conj().T
+    elif Px_comp is None and X is None: 
+        raise('at least one of X or Px_comp should be given')
+    return X_comp.conj().T @ Px_comp @ Z
+
+def get_tangent_parametrization(X:np.ndarray, Z:np.ndarray, X_comp:np.ndarray = None ,stack:bool=True):
+    """
+    Get the A and B matrices that parametrize the Z matrix of the tangent space of stiefel manifold at X
+    
+    Z = X @ A + X_comp @ B
+    
+    with X_comp the orthogonal complement of X.
+
+    From https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=984753 lemma 8.
+    """
+    if X_comp is None:
+        X_comp = get_orthogonal_complement(X)
+
+    A = get_symmetric(X, Z)
+    B = get_arbitrary(X_comp=X_comp, Z=Z, X=X)
+    if stack:
+        return np.vstack([A,B])
+    else:
+        return A, B
 
 def polar_decomposition_stiefel(X, Z):
     """
@@ -86,7 +158,15 @@ def is_in_tangent_space(X:np.ndarray, Z:np.ndarray):
     "checks if the matrix Z is in the tangent space of isometry X"
     assert X.ndim == Z.ndim == 2, "only matrices allowed"
     d = X.shape[1]
-    return np.allclose(X.conj().T @ Z + Z.conj().T @ X, np.zeros((d,d)))
+    return np.allclose(X.conj().T @ Z, - Z.conj().T @ X)
+
+def is_antisymmetric(A:np.ndarray):
+    "checks if a matrix is antisymmetric or not"
+    return np.allclose(A, -A.conj().T)
+
+def is_symmetric(C:np.ndarray):
+    "checks if a matrix is symmetric or not"
+    return np.allclose(C, C.conj().T)
 
 def gradient_stiefel(xi, func):
     "compute riemannian gradient for all xi, returning a list"
@@ -96,9 +176,10 @@ def gradient_stiefel(xi, func):
 
 def gradient_stiefel_vec(xi, func):
     "compute the vectorized gradient for all xi"
+    # NOTE: changing to store only parametrization here to adhere to trust_region function 
     return jnp.vstack([
-            vectorize(grad) 
-    for grad in gradient_stiefel(xi, func)]).reshape(-1)
+        get_tangent_parametrization(X=x, Z=grad, stack=True) 
+    for grad, x in zip(gradient_stiefel(xi, func), xi)]).reshape(-1)
 
 
 def riemannian_hessian(func, x, vector=False):
@@ -121,14 +202,15 @@ def riemannian_hessian(func, x, vector=False):
             # printing every 100 steps to see progress
             # if k%100 == 0:
                 # print('element: ', k)
-            _, jvp_eval = jax.jvp(grad_func, (x,), ([jnp.zeros_like(op) if l!=i else jnp.roll(unit_matrices[l],k) for l,op in enumerate(x)],))
+            _, jvp_eval = jax.jvp(grad_func, (x,), ([jnp.zeros_like(op) if l!=i else project(X=op,Z=jnp.roll(unit_matrices[l],k)) for l,op in enumerate(x)],))
 
             for j, element in enumerate(jvp_eval):
                 # we need to project each of them and store in an array that has information about the index k
-                if vector:                    
-                    xi_dxk[j][:,k] += project(x[j],element).reshape(-1)
+                if vector:            
+                    # NOTE: changing to store only parametrization here to adhere to trust_region function 
+                    xi_dxk[j][:,k] +=  get_tangent_parametrization(X=x[j], Z=project(x[j],element), stack=True).reshape(-1)
                 else:
-                    xi_dxk[j][:,:,k] += project(x[j],element)
+                    xi_dxk[j][:,:,k] += get_tangent_parametrization(X=x[j], Z=project(x[j],element), stack=True)
         hessian_columns.append(xi_dxk)
     return hessian_columns
 