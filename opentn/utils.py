d7496754194cc75758f59789666a9f7dec6aeb9d
EmilianoG-byte <57567043+EmilianoG-byte@users.noreply.github.com>
2023-11-14 00:00:27 +0100

    update utils plot discrete

--- opentn/utils.py
+++ opentn/utils.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import numpy as np
 
-def set_discrete_labels(labels:list[str], ax=None):
+def set_discrete_labels(labels:list[str], ax=None, rotation=65):
+    "must be called after plotting the data (e.g. right before `plt.legend()`)"
+    # NOTE: it does not work if we used an x array when plotting
     if ax is None:
         ax = plt.gca()
     ax.xaxis.set_ticks(np.arange(len(labels)))
-    ax.xaxis.set_ticklabels(labels, rotation=65)
+    ax.xaxis.set_ticklabels(labels, rotation=rotation)
 
 def set_integer_labels(ax=None):
     "must be called after plotting the data (e.g. right before `plt.legend()`)"
